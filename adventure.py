72c0117326ae10bdba912a3f9b812e6bd694ae12
Mike Rooney <mike.rooney.nz@hotmail.com>
2022-10-29 17:01:05 +1300

    Simplified items system

--- adventure.py
+++ adventure.py
@@ -8,24 +8,15 @@ rooms = {
             },
         "Dining Room": {
             "exits": {"S": "Foyer"},
-            "items": "knife"
+            "items": ("knife")
             },
         "East Wing": {
-            "exits": {"W": "Foyer"}
+            "exits": {"W": "Foyer"},
+            "items": ()
             }
         }
 
 current_room=next(iter(rooms)) #first key in dict == first room
-
-def values_or_none(a_dict, a_key):
-    """ wrapper around dict so that asking for values with
-        a key that doesn't exist will return None instead
-        of raising an exception
-    """
-    if a_key in a_dict:
-        return a_dict[a_key]
-    else:
-        return None
 
 def exits_from_room(room_name):
     """ returns the exits available from a room """
@@ -38,9 +29,8 @@ def destination_from_room_exit(room_name, direction):
 
 def items_in_room(room_name):
     """ reutrns the list of items in a room,
-        or None if there aren't any
-    """
-    return values_or_none(rooms[current_room],"items")
+        or None if there aren't any """
+    return rooms[current_room]["items"]
 
 if __name__ == "__main__":
     print("Good luck adventurer - commands are N, S, W, E and Q to quit.")
@@ -48,7 +38,8 @@ if __name__ == "__main__":
         print()
         print("you are in ",current_room)
         print("exits are ",list(exits_from_room(current_room)))
-        print("items here are ",items_in_room(current_room))
+        if items_in_room(current_room) != ():
+            print("items here are ",items_in_room(current_room))
         user_command = input("your command >")
         if user_command in exits_from_room(current_room):
             current_room = destination_from_room_exit(current_room, user_command)
