9f5bcc65b259fe1abeb85f09d8687e81a9d852bb
Mike Rooney <mike.rooney.nz@hotmail.com>
2022-10-29 17:38:40 +1300

    Further simplified

--- adventure.py
+++ adventure.py
@@ -12,37 +12,23 @@ rooms = {
             },
         "East Wing": {
             "exits": {"W": "Foyer"},
-            "items": ()
             }
         }
 
-current_room=next(iter(rooms)) #first key in dict == first room
-
-def exits_from_room(room_name):
-    """ returns the exits available from a room """
-    return rooms[room_name]["exits"].keys()
-
-def destination_from_room_exit(room_name, direction):
-    """ returns the destination room given a starting room
-        and a direction """
-    return rooms[room_name]["exits"][direction]
-
-def items_in_room(room_name):
-    """ reutrns the list of items in a room,
-        or None if there aren't any """
-    return rooms[current_room]["items"]
+current_room="Foyer"
 
 if __name__ == "__main__":
     print("Good luck adventurer - commands are N, S, W, E and Q to quit.")
     while current_room != "end_room":
         print()
         print("you are in ",current_room)
-        print("exits are ",list(exits_from_room(current_room)))
-        if items_in_room(current_room) != ():
-            print("items here are ",items_in_room(current_room))
+        if "exits" in rooms[current_room].keys():
+            print("exits are ",list(rooms[current_room]["exits"].keys()))
+        if "items" in rooms[current_room].keys():
+            print("items here are ",rooms[current_room]["items"])
         user_command = input("your command >")
-        if user_command in exits_from_room(current_room):
-            current_room = destination_from_room_exit(current_room, user_command)
+        if user_command in rooms[current_room]["exits"].keys():
+            current_room = rooms[current_room]["exits"][user_command]
         elif user_command == "Q":
             exit()
         else:
